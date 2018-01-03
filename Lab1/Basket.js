d5697e4ba1ff0d3b67c4f935618978c7b4621854
ArtemKit <34881781+ArtemKit@users.noreply.github.com>
2018-01-03 16:22:48 +0200

    Update Basket.js

--- Lab1/Basket.js
+++ Lab1/Basket.js
@@ -9,26 +9,57 @@ var prodcuts = [{
 }];
 
 class ProductLineItem {
+ 	
 	constructor(product) {
-		//implement
+		this.amount = 1;
+		this.product = product;
 	}
+	
+	 setAmount(quantity) {
+		this.amount = quantity;
+	 }
+	
+	
+	
 }
 
 
 var basket = (function(){
-	//implement
+	var a = []
 	return {
 		addProduct : function(productID){
-			//implement
+			if(a[productID] == null) {
+				if ( prodcuts[productID].inventory-1 >= 0) {
+					a[productID] = new ProductLineItem(prodcuts[productID]);
+					a[productID].product.inventory--;
+				} else {
+					alert("Немає продуктів на складі!");
+				}
+			} else {
+				alert("Ви вже вибрали цей продукт");
+			}
+				
 		},
 		removeProduct : function(productID){
-			//implement
+			prodcuts[productID].inventory = prodcuts[productID].inventory + a[productID].amount;
+			delete a[productID];
 		},
 		updateProductQuantity : function(productID, quantity) {
-			//implement
+			if ( a[productID].getProduct().inventory-quantity+ a[productID].getAmount() >= 0) {
+				a[productID].product.inventory = a[productID].product.inventory - quantity + a[productID].amount;
+				a[productID].setAmount(quantity);
+			} else {
+				alert("Немає продуктів на складі!");
+			}
 		},
 		getTotalPrice : function(){
-			//implement
+			var totalPrice = 0;
+			a.forEach(function(item, i, arr) {
+				if ( item != null) {
+					totalPrice += item.product.price * item.amount;
+				}
+			});
+			return totalPrice;
 		}
 	}
 })();
